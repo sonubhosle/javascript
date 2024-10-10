61c8b50c0f2e447e31e41d3c53853ad473f8be0f
sonubhosle <sbhosle1011@gmail.com>
2024-10-10 13:15:44 +0530

    first commit

---
+++ Basic/script.js
@@ -0,0 +1,8 @@
+// Data Types
+// var,let,const
+// console.log,error,prompt,alert
+// function keyword ,function name 
+// simple brackets,function calling
+// prompt('Enter Your Name');
+// console.log(prompt)
+
