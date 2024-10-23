b85dc2a5d04185083819eb063f89124f21f2b4f6
can-helpyou <icanhelpyou2808@gmail.com>
2024-10-23 14:19:53 +0530

    23 Oct

---
+++ Oprations On Array/Array.js
@@ -0,0 +1,26 @@
+// Define Array
+
+const fruits = ["Mang", "strawberry", "Apple", "Lemon"];
+console.log(fruits)
+
+// Read Index 
+
+let firstElement = fruits[2]
+console.log(firstElement)
+
+// Chnage Elements
+
+ fruits[0] = "Mango"
+ console.log(fruits)
+
+// inserting And Deleting
+
+ fruits.push("Onion")
+ console.log(fruits)
+
+ fruits.pop("Onion")
+ console.log(fruits)
+ 
+// Slice Items By Index
+ const min =  fruits.slice(0,3)
+ console.log(min)
